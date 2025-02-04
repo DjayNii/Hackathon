7344f1ebb5187f9d829c8363a3c0668a98cf0515
DjayNii <nithinreddyk24@gmail.com>
2025-02-05 00:42:10 +0530

    added router

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,20 +1,22 @@
-import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
 import Login from "./components/Login";
 import Signup from "./components/SignUp";
 import HomePage from "./components/HomePage";
-import QuestionBuilder from "./components/QuestionMaker";
+
 import FormBuilder from "./components/FormBuilder";
 import FormView from "./components/FormView";
 
 function App() {
-
   return (
     <>
-      {/* <Login></Login> */}
-      {/* <Signup></Signup> */}
-      {/* <HomePage></HomePage> */}
-      <FormBuilder></FormBuilder>
-      {/* <FormView></FormView> */}
+      <Routes>
+        <Route path="/login" element={<Login></Login>} />
+        <Route path="/" element={<Login></Login>} />
+        <Route path="/signup" element={<Signup></Signup>} />
+        <Route path="/home" element={<HomePage></HomePage>} />
+        <Route path="/formBuilder" element={<FormBuilder></FormBuilder>} />
+        <Route path="/formView" element={<FormView></FormView>} />
+      </Routes>
     </>
   );
 }
